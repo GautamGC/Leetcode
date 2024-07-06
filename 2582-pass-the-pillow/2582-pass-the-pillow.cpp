ff97141638300d7e0e4b6c03d7ad267ff27fc723
Gautam Chauhan <gautamchauhan.officialgc@gmail.com>
2024-07-06 22:39:18 +0530

    Time: 1 ms (54.64%), Space: 7.1 MB (71.18%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  int passThePillow(int n, int time) {
+    time %= (n - 1) * 2;
+    if (time < n){
+      return 1 + time;
+    }
+    return n - (time - (n - 1)); 
+  }
+};
